52f520d0ad5e06e5dc4e677c8a5a18be04572298
Keir Little <me@keirlittle.com>
2025-03-23 12:07:07 +0000

    Add uwsgi

--- hannou/deploy.py
+++ hannou/deploy.py
@@ -7,3 +7,9 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 ALLOWED_HOSTS = ["127.0.0.1"]
 MEDIA_ROOT = BASE_DIR / "media"
 DJANGO_VITE["default"]["dev_mode"] = False
+LOGGING["handlers"]["file"] = {
+    "level": "DEBUG",
+    "class": "logging.FileHandler",
+    "filename": BASE_DIR / "hannou.log",
+}
+LOGGING["loggers"]["hannou"]["handlers"].append("file")
